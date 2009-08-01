cd585d9dcdb06ac1924f53e682d90a9dd49dca20
markpollack <mark.pollack@springsource.com>
2009-08-01 06:37:39 +0000

    SPRNET-843 - VS.NET 2008 project and solutions templates

---
+++ dev-support/vs.net-2008/install-templates.bat
@@ -0,0 +1,13 @@
+".\templates\solution\AdoSolution\ADO.NET Class Library.msi"
+
+copy ".\templates\solution\AdoSolution\ADO.NET Class Library.zip" "%userprofile%\My Documents\Visual Studio 2008\Templates\ProjectTemplates\CSharp\Spring.NET"
+
+copy ".\templates\solution\ClassLibrarySolution\Class Library.zip" "%userprofile%\My Documents\Visual Studio 2008\Templates\ProjectTemplates\CSharp\Spring.NET"
+
+copy ".\templates\solution\NHibernateSolution\NHibernate Class Library.zip" "%userprofile%\My Documents\Visual Studio 2008\Templates\ProjectTemplates\CSharp\Spring.NET"
+
+copy ".\templates\solution\WebSolution\ASP.NET Application.zip" "%userprofile%\My Documents\Visual Studio 2008\Templates\ProjectTemplates\CSharp\Spring.NET"
+
+
+
+
