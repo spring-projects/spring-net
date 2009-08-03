64ff3178a60005a9c3b8002c60d966d7aa42295b
eeichinger <eeichinger@gmail.com>
2009-08-03 00:28:51 +0000

    upgraded Spring.Data.NHibernate20.Integration.Tests to test.integration.data

--- test/Spring/Spring.Data.NHibernate20.Integration.Tests/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-using System.Reflection;
-using System.Runtime.CompilerServices;
-
-[assembly: AssemblyTitle("Spring.Data.NHibernate20 Integration Tests")]
-[assembly: AssemblyDescription("Integration tests for Spring.Data.NHibernate20 assembly")]
