3dc044203c2abf9c56106801e21fd11a5653a464
bbaia <brbaia@gmail.com>
2010-07-21 14:33:15 +0000

    Add 'side by side' support in Framework .NET 4.0 [SPRNET-1320]

--- test/Spring/Spring.Core.Tests/AssemblyInfo.cs
+++ test/Spring/Spring.Core.Tests/AssemblyInfo.cs
@@ -1,5 +1,8 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 
+#if NET_4_0
+[assembly: System.Security.SecurityRules(System.Security.SecurityRuleSet.Level1)] 
+#endif
 [assembly: AssemblyTitle("Spring.Core Tests")]
 [assembly: AssemblyDescription("Unit tests for Spring.Core assembly")]
