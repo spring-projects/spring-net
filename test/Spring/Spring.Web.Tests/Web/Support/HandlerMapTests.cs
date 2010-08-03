b351bc2f21a8b368310d6d562c82f67ae5fd474b
sbohlen <sbohlen@gmail.com>
2010-08-03 15:14:41 +0000

    SPRNET-1271 -cannot reproduce -added unit tests around the HandlerMap class' MapPath() method so that if a subsequent example of the behavior reported in this issue is provided we can properly test against it

---
+++ test/Spring/Spring.Web.Tests/Web/Support/HandlerMapTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text;
+using NUnit.Framework;
+
+namespace Spring.Web.Support
+{
+    [TestFixture]
+    public class HandlerMapTests
+    {
+        [Test]
+        public void CanMatch_EmbeddedLiteralsPattern()
+        {
+            HandlerMap map = new HandlerMap();
+            map.Add("/*/test2/*", "theHandlerIWant");
+
+            HandlerMapEntry handler = map.MapPath("/test1/test2/default.aspx");
+
+            Assert.NotNull(handler);
+            Assert.AreEqual("theHandlerIWant", handler.HandlerObjectName);
+        }
+
+        [Test]
+        public void CanMatch_ExactStringPatterns()
+        {
+            HandlerMap map = new HandlerMap();
+            map.Add("/test1/test2/default.aspx", "theHandlerIWant");
+
+            HandlerMapEntry handler = map.MapPath("/test1/test2/default.aspx");
+
+            Assert.NotNull(handler);
+            Assert.AreEqual("theHandlerIWant", handler.HandlerObjectName);
+        }
+
+        [Test]
+        public void CanMatch_LeadingLiteralsPattern()
+        {
+            HandlerMap map = new HandlerMap();
+            map.Add("/test1/*", "theHandlerIWant");
+
+            HandlerMapEntry handler = map.MapPath("/test1/test2/default.aspx");
+
+            Assert.NotNull(handler);
+            Assert.AreEqual("theHandlerIWant", handler.HandlerObjectName);
+        }
+
+    }
+}
