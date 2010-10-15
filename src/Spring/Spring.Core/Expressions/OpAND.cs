5c4ad017a99d347d5a50d90e785d07d262355e66
bbaia <brbaia@gmail.com>
2010-10-15 12:34:03 +0000

    short-circuit evaluation of 'AND' logic operator in SpEl [SPRNET-1381]

--- src/Spring/Spring.Core/Expressions/OpAND.cs
+++ src/Spring/Spring.Core/Expressions/OpAND.cs
@@ -63,23 +63,31 @@ namespace Spring.Expressions
         protected override object Get(object context, EvaluationContext evalContext)
         {
             object l = GetLeftValue(context, evalContext);
-            object r = GetRightValue(context, evalContext);
 
-            if (NumberUtils.IsInteger(l) && NumberUtils.IsInteger(r))
+            if (NumberUtils.IsInteger(l))
             {
-                return NumberUtils.BitwiseAnd(l, r);
+                object r = GetRightValue(context, evalContext);
+                if (NumberUtils.IsInteger(r))
+                {
+                    return NumberUtils.BitwiseAnd(l, r);
+                }
             }
-            else if (l is Enum && l.GetType() == r.GetType())
+            else if (l is Enum)
             {
-                Type enumType = l.GetType();
-                Type integralType = Enum.GetUnderlyingType(enumType);
-                l = Convert.ChangeType(l, integralType);
-                r = Convert.ChangeType(r, integralType);
-                object result = NumberUtils.BitwiseAnd(l, r);
-                return Enum.ToObject(enumType, result);
+                object r = GetRightValue(context, evalContext);
+                if (l.GetType() == r.GetType())
+                {
+                    Type enumType = l.GetType();
+                    Type integralType = Enum.GetUnderlyingType(enumType);
+                    l = Convert.ChangeType(l, integralType);
+                    r = Convert.ChangeType(r, integralType);
+                    object result = NumberUtils.BitwiseAnd(l, r);
+                    return Enum.ToObject(enumType, result);
+                }
             }
-            else
-                return Convert.ToBoolean(l) && Convert.ToBoolean(r);
+
+            return Convert.ToBoolean(l) &&
+                Convert.ToBoolean(GetRightValue(context, evalContext));
         }
     }
 }
