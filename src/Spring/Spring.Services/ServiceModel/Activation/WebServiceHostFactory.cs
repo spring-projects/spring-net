62b0591321a1958878e18ead11a572cdafe5027f
sbohlen <sbohlen@gmail.com>
2010-11-01 19:48:37 +0000

    SPRNET-1354 Introduced WebServiceHostFactory and added .NET 3.5 build

---
+++ src/Spring/Spring.Services/ServiceModel/Activation/WebServiceHostFactory.cs
@@ -0,0 +1,76 @@
+#if NET_3_5
+#region License
+
+/*
+ * Copyright © 2002-2008 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#endregion
+
+#region Imports
+
+using System;
+using System.ServiceModel;
+
+using Spring.Util;
+using Spring.Context.Support;
+using Spring.Context;
+
+#endregion
+
+namespace Spring.ServiceModel.Activation
+{
+    /// <summary>
+    /// Factory that provides instances of <see cref="Spring.ServiceModel.SpringWebServiceHost"/>
+    /// to host objects created by Spring's IoC container.
+    /// </summary>
+    /// <author>Steve Bohlen</author>
+    public class WebServiceHostFactory : System.ServiceModel.Activation.WebServiceHostFactory
+    {
+        /// <summary>
+        /// Creates a <see cref="Spring.ServiceModel.SpringWebServiceHost"/> for 
+        /// a specified Spring-managed object with a specific base address.
+        /// </summary>
+        /// <param name="reference">
+        /// A reference to a Spring-managed object or to a service type.
+        /// </param>
+        /// <param name="baseAddresses">
+        /// The <see cref="System.Array"/> of type <see cref="System.Uri"/> that contains 
+        /// the base addresses for the service hosted.
+        /// </param>
+        /// <returns>
+        /// A <see cref="Spring.ServiceModel.SpringWebServiceHost"/> for the Spring-managed object.
+        /// </returns>
+        /// <exception cref="System.ArgumentException">
+        /// If the Service attribute in the ServiceHost directive was not provided.
+        /// </exception>
+        public override ServiceHostBase CreateServiceHost(string reference, Uri[] baseAddresses)
+        {
+            if (StringUtils.IsNullOrEmpty(reference))
+            {
+                return base.CreateServiceHost(reference, baseAddresses);
+            }
+
+            IApplicationContext applicationContext = ContextRegistry.GetContext();
+            if (applicationContext.ContainsObject(reference))
+            {
+                return new SpringWebServiceHost(reference, applicationContext, baseAddresses);
+            }
+
+            return base.CreateServiceHost(reference, baseAddresses);
+        }
+    }
+}
+#endif
---
+++ src/Spring/Spring.Services/ServiceModel/SpringWebServiceHost.cs
@@ -0,0 +1,119 @@
+﻿#if NET_3_5
+#region License
+
+/*
+ * Copyright © 2002-2008 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#endregion
+
+#region Imports
+
+using System;
+using Spring.Util;
+using Spring.Context;
+using Spring.Context.Support;
+using Spring.ServiceModel.Support;
+using Spring.Objects.Factory;
+using System.ServiceModel.Web;
+
+#endregion
+
+namespace Spring.ServiceModel
+{
+    /// <summary>
+    /// Provides a host for Spring-managed services.
+    /// </summary>
+    /// <author>Bruno Baia</author>
+    public class SpringWebServiceHost : WebServiceHost
+    {
+        #region Constructor(s) / Destructor
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="Spring.ServiceModel.SpringWebServiceHost"/> class.
+        /// </summary>
+        /// <param name="serviceName">The name of the service within Spring's IoC container.</param>
+        /// <param name="baseAddresses">The base addresses for the hosted service.</param>
+        public SpringWebServiceHost(string serviceName, params Uri[] baseAddresses)
+            : this(serviceName, GetApplicationContext(null), baseAddresses)
+        {
+        }
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="Spring.ServiceModel.SpringWebServiceHost"/> class.
+        /// </summary>
+        /// <param name="serviceName">The name of the service within Spring's IoC container.</param>
+        /// <param name="contextName">The name of the Spring context to use.</param>
+        /// <param name="baseAddresses">The base addresses for the hosted service.</param>
+        public SpringWebServiceHost(string serviceName, string contextName, params Uri[] baseAddresses)
+            : this(serviceName, GetApplicationContext(contextName), baseAddresses)
+        {
+        }
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="Spring.ServiceModel.SpringWebServiceHost"/> class.
+        /// </summary>
+        /// <param name="serviceName">The name of the service within Spring's IoC container.</param>
+        /// <param name="objectFactory">The <see cref="IObjectFactory"/> to use.</param>
+        /// <param name="baseAddresses">The base addresses for the hosted service.</param>
+        public SpringWebServiceHost(string serviceName, IObjectFactory objectFactory, params Uri[] baseAddresses)
+            : this(serviceName, objectFactory, true, baseAddresses)
+        {
+        }
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="Spring.ServiceModel.SpringWebServiceHost"/> class.
+        /// </summary>
+        /// <param name="serviceName">The name of the service within Spring's IoC container.</param>
+        /// <param name="objectFactory">The <see cref="IObjectFactory"/> to use.</param>
+        /// <param name="useServiceProxyTypeCache">Whether to cache the generated service proxy type.</param>
+        /// <param name="baseAddresses">The base addresses for the hosted service.</param>
+        public SpringWebServiceHost(string serviceName, IObjectFactory objectFactory, bool useServiceProxyTypeCache, params Uri[] baseAddresses)
+            : base(CreateServiceType(serviceName, objectFactory, useServiceProxyTypeCache), baseAddresses)
+        {
+        }
+
+        private static IApplicationContext GetApplicationContext(string contextName)
+        {
+            if (StringUtils.IsNullOrEmpty(contextName))
+            {
+                return ContextRegistry.GetContext();
+            }
+            else
+            {
+                return ContextRegistry.GetContext(contextName);
+            }
+        }
+
+        private static Type CreateServiceType(string serviceName, IObjectFactory objectFactory, bool useServiceProxyTypeCache)
+        {
+            if (StringUtils.IsNullOrEmpty(serviceName))
+            {
+                throw new ArgumentException("The service name cannot be null or an empty string.", "serviceName");
+            }
+
+            if (objectFactory.IsTypeMatch(serviceName, typeof(Type)))
+            {
+                return objectFactory.GetObject(serviceName) as Type;
+            }
+
+            return new ServiceProxyTypeBuilder(serviceName, objectFactory.GetType(serviceName), useServiceProxyTypeCache)
+                .BuildProxyType(objectFactory);                    
+        }
+
+        #endregion
+    }
+}
+#endif
