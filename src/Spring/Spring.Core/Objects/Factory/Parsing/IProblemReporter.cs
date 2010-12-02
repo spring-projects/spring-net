87a9d293da8126390867235872a5c5d3b95c2edc
markpollack <mark.pollack@springsource.com>
2010-12-02 22:34:22 +0000

    Add missing file from code config branch

---
+++ src/Spring/Spring.Core/Objects/Factory/Parsing/IProblemReporter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Spring.Objects.Factory.Parsing
+{
+    public interface IProblemReporter
+    {
+        void Fatal(Problem problem);
+        void Warning(Problem problem);
+        void Error(Problem problem);
+    }
+}
