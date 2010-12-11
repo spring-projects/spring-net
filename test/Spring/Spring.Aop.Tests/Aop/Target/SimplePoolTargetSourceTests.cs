0e3f54715361e930bceaa1eb3f322609da19116f
sbohlen <sbohlen@gmail.com>
2010-12-11 17:28:45 +0000

    SPRNET-1406 Copyright notices updated globally to reflect consistent date-range references.

--- test/Spring/Spring.Aop.Tests/Aop/Target/SimplePoolTargetSourceTests.cs
+++ test/Spring/Spring.Aop.Tests/Aop/Target/SimplePoolTargetSourceTests.cs
@@ -3,7 +3,7 @@ using Spring.Aop.Target;
 using Spring.Objects.Factory;
 using Spring.Objects.Factory.Xml;
 /*
-* Copyright 2002-2004 the original author or authors.
+* Copyright 2002-2010 the original author or authors.
 * 
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
--- test/Spring/Spring.Core.Tests/Threading/LatchTest.cs
+++ test/Spring/Spring.Core.Tests/Threading/LatchTest.cs
@@ -6,7 +6,7 @@ using NUnit.Framework;
 #region Licence
 
 /*
- * Copyright 2002-2004 the original author or authors.
+ * Copyright 2002-2010 the original author or authors.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
