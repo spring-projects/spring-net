5532f67a9dc3368532c5a7b1f2d23eba8fa65a40
bbaia <brbaia@gmail.com>
2011-02-04 19:10:36 +0000

    Removed Spring.Http project (moved to GitHub project spring-net-rest) (SPRNET-1345)

--- src/Spring/Spring.Http/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-using System;
-using System.Reflection;
-
-[assembly: AssemblyTitle("Spring.Http")]
-[assembly: AssemblyDescription("Interfaces and classes that provide REST client API in Spring.NET")]
