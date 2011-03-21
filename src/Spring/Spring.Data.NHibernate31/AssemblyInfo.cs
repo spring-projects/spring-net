3dd72e856249060b9870bdadd434644a0892bf30
lahma <marko.lahma@gmail.com>
2011-03-21 08:25:14 +0000

    SPRNET-1421: NHibernate 3.1 support

---
+++ src/Spring/Spring.Data.NHibernate31/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+using System;
+using System.Reflection;
+
+[assembly: AssemblyTitle("Spring.Net NHibernate 3.1 support")]
+[assembly: AssemblyDescription("Interfaces and classes that provide NHibernate 3.1 support in Spring.Net")]
---
+++ test/Spring/Spring.Data.NHibernate31.Tests/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle("Spring.Data.NHibernate31 Integration Tests")]
+[assembly: AssemblyDescription("Integration tests for Spring.Data.NHibernate31 assembly")]
