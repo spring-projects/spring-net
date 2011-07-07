79f397b4a3dca991ecbb6e3dbc409b4924ddfef3
sbohlen <sbohlen@gmail.com>
2011-07-07 17:20:13 +0000

    SPRNET-1454 -add support for NH 3.2

---
+++ test/Spring/Spring.Data.NHibernate32.Integration.Tests/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+using System.Reflection;
+
+[assembly: AssemblyTitle("Spring.Data.NHibernate32 Integration Tests")]
+[assembly: AssemblyDescription("Integration tests for Spring.Data.NHibernate32 assembly")]
