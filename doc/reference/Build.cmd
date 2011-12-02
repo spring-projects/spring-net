b72e9eb50fc336b05d9e2a1612ea98c8041755cf
Marijn van der Zee <marijn@serraict.com>
2011-12-02 14:27:53 +0100

    Print out buildlog.txt to console

--- doc/reference/Build.cmd
+++ doc/reference/Build.cmd
@@ -2,7 +2,8 @@
 @echo ...
 @echo Running reference documentation Build Script, capturing output to buildlog.txt ...
 @echo Start Time: %time%
-..\..\build-support\tools\nant\bin\nant %1 %2 %3 %4 %5 %6 %7 %8 %9 > buildlog.txt
+..\..\build-support\tools\nant\bin\nant %1 %2 %3 %4 %5 %6 %7 %8 %9 > buildlog.txt 
+type buildlog.txt
 @echo ...
 @echo ************************
 @echo Build Complete!
