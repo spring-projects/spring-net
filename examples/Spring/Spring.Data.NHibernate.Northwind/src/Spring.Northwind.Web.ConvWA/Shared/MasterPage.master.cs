eb22eb1214a0d4d99b3024c9a250a62a743245b0
Steve Bohlen <sbohlen@gmail.com>
2012-12-27 17:51:22 -0500

    renaming conversation-based example project

--- examples/Spring/Spring.Data.NHibernate.Northwind/src/Spring.Northwind.Web.ConvWA/Shared/MasterPage.master.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-public partial class Shared_MasterPage : System.Web.UI.MasterPage
-{
-    protected void Page_Load(object sender, EventArgs e)
-    {
-
-    }
-}
---
+++ examples/Spring/Spring.Data.NHibernate.Northwind/src/Spring.Northwind.Web.Conversation/Default.aspx.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Diagnostics;
+using Spring.Northwind.Dao;
+using Spring.Northwind.Domain;
+using Spring.Northwind.Service;
+
+public partial class _Default : ConversationPage
+{
+    private ICustomerDao customerDao;
+    private IFulfillmentService fulfillmentService;
+
+    public IFulfillmentService FulfillmentService
+    {
+        set { this.fulfillmentService = value; }
+    }
+
+    public ICustomerDao CustomerDao
+    {
+        set { this.customerDao = value; }
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        ProcessCustomer();
+    }
+
+    public void ProcessCustomer()
+    {
+        string customerId = "ERNSH";
+
+        // check, if exists
+        Customer customer = customerDao.Get(customerId);
+
+        //Find all orders for customer and ship them
+        this.fulfillmentService.ProcessCustomer(customer.Id);
+        //assertions....
+
+        // prepare for display - note that OrderDetails are loaded lazy here!
+        foreach (Order order in customer.Orders)
+        {
+            Debug.Assert(order.OrderDetails.Count > 0);
+            foreach(OrderDetail details in order.OrderDetails)
+            {
+                details.Quantity = details.Quantity - 1;
+                // do something here
+            }
+        }
+    }
+    protected void customerList_Click(object sender, EventArgs e)
+    {
+        SetResult("CustomerList");
+    }
+}
