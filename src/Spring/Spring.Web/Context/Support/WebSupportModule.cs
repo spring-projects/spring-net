a835e31ad2c5496c2135bfb685176d2fc8d7ac48
Andreas Kluth <mail@andreaskluth.net>
2013-11-28 23:50:03 +0100

    SPRNET-1387 - Minor refactorings/cleanups while analyzing the issue.

--- src/Spring/Spring.Web/Context/Support/WebSupportModule.cs
+++ src/Spring/Spring.Web/Context/Support/WebSupportModule.cs
@@ -1,7 +1,7 @@
 #region License
 
 /*
- * Copyright © 2002-2011 the original author or authors.
+ * Copyright © 2002-2013 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -292,7 +292,7 @@ namespace Spring.Context.Support
                 }
                 else
                 {
-                    // this is an async session timout - log as fatal since this is the thread's exit point!
+                    // this is an async session timeout - log as fatal since this is the thread's exit point!
                     s_log.Fatal(msg, ex);
                 }
             }
