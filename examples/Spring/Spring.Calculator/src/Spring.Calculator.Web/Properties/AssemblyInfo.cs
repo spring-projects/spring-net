3c1bbe0e2f193df2c61a176f9e44a4e3b636a13c
Marko Lahma <marko.lahma@gmail.com>
2014-01-06 20:44:36 +0200

    add AssemblyInfo.cs to Spring.Calculator.Web.2010.csproj to avoid MsBuild bug with no CS files in project

---
+++ examples/Spring/Spring.Calculator/src/Spring.Calculator.Web/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Spring.Calculator")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Spring.Calculator")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
