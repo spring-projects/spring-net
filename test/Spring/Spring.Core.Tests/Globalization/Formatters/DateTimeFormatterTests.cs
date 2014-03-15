991a8b1bd108c141cfda19649161a2c6d4e3a9ec
sbohlen <sbohlen@gmail.com>
2014-03-15 09:12:02 -0400

    add discriminator to respect CLR version (in addition to OS version) as a factor in choosing which FORMATTER-related expectations to test against

--- test/Spring/Spring.Core.Tests/Globalization/Formatters/DateTimeFormatterTests.cs
+++ test/Spring/Spring.Core.Tests/Globalization/Formatters/DateTimeFormatterTests.cs
@@ -68,7 +68,10 @@ namespace Spring.Globalization.Formatters
 
             fmt = new DateTimeFormatter("D", CultureInfoUtils.SerbianLatinCultureName);
 
-            if (CultureInfoUtils.OperatingSystemIsLaterThanWindows7)
+            Console.WriteLine("Steve:  " + Environment.Version);
+
+
+            if (CultureInfoUtils.OperatingSystemIsLaterThanWindows7 && CultureInfoUtils.ClrIsVersion4OrLater)
             {
                 Assert.AreEqual("14. avgust 2004.", fmt.Format(new DateTime(2004, 8, 14)));
                 Assert.AreEqual("24. avgust 1974.", fmt.Format(new DateTime(1974, 8, 24)));
@@ -81,7 +84,7 @@ namespace Spring.Globalization.Formatters
 
             fmt = new DateTimeFormatter("dd-MMM-yyyy", CultureInfoUtils.SerbianCyrillicCultureName);
 
-            if (CultureInfoUtils.OperatingSystemIsLaterThanWindows7)
+            if (CultureInfoUtils.OperatingSystemIsLaterThanWindows7 && CultureInfoUtils.ClrIsVersion4OrLater)
             {
                 Assert.AreEqual("14-авг.-2004", fmt.Format(new DateTime(2004, 8, 14)));
                 Assert.AreEqual("24-авг.-1974", fmt.Format(new DateTime(1974, 8, 24)));
@@ -107,7 +110,7 @@ namespace Spring.Globalization.Formatters
 
             fmt = new DateTimeFormatter("D", CultureInfoUtils.SerbianLatinCultureName);
 
-            if (CultureInfoUtils.OperatingSystemIsLaterThanWindows7)
+            if (CultureInfoUtils.OperatingSystemIsLaterThanWindows7 && CultureInfoUtils.ClrIsVersion4OrLater)
             {
                 Assert.AreEqual(new DateTime(2004, 8, 14), fmt.Parse("14. avgust 2004."));
                 Assert.AreEqual(new DateTime(1974, 8, 24), fmt.Parse("24. avgust 1974."));
@@ -120,7 +123,7 @@ namespace Spring.Globalization.Formatters
 
             fmt = new DateTimeFormatter("dd-MMM-yyyy", CultureInfoUtils.SerbianCyrillicCultureName);
 
-            if (CultureInfoUtils.OperatingSystemIsLaterThanWindows7)
+            if (CultureInfoUtils.OperatingSystemIsLaterThanWindows7 && CultureInfoUtils.ClrIsVersion4OrLater)
             {
                 Assert.AreEqual(new DateTime(2004, 8, 14), fmt.Parse("14-авг.-2004"));
                 Assert.AreEqual(new DateTime(1974, 8, 24), fmt.Parse("24-авг.-1974"));
