2bfe447075bb0082519aad608726ecb93ba421ba
Steve Bohlen <sbohlen@gmail.com>
2015-01-16 22:19:00 -0500

    add missing closing quotation in sqlcmd arguments

--- build-support/appveyor-database-integration-tests-setup-scripts.cmd
+++ build-support/appveyor-database-integration-tests-setup-scripts.cmd
@@ -2,7 +2,7 @@ rem scripts to setup integration test database on CI server
 rem NOTE: sqlcmd params are hard-coded to credentials and instance name for SQL Server 2012 as per http://www.appveyor.com/docs/services-databases
 
 rem first, create the databases and the SpringQA user
-sqlcmd -S ".\SQL2012SP1" -U sa -P Password12! -i "%APPVEYOR_BUILD_FOLDER%\build-support\create-integration-test-databases-and-users.sql
+sqlcmd -S ".\SQL2012SP1" -U sa -P Password12! -i "%APPVEYOR_BUILD_FOLDER%\build-support\create-integration-test-databases-and-users.sql"
 
 rem add the 'Spring' schema and data
 sqlcmd -S ".\SQL2012SP1" -U sa -P Password12! -i "%APPVEYOR_BUILD_FOLDER%\test\Spring\Spring.Data.Integration.Tests\Data\Spring.Data.Integration.Tests_Spring_database.sql"
