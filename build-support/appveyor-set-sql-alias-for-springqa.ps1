21d2ac8fab45275abb5c391e4633960315058631
Steve Bohlen <sbohlen@gmail.com>
2015-01-17 15:05:47 -0500

    comment back in missing named-piped config value

--- build-support/appveyor-set-sql-alias-for-springqa.ps1
+++ build-support/appveyor-set-sql-alias-for-springqa.ps1
@@ -26,7 +26,7 @@ if ((test-path -path $x64) -ne $True)
  
 #Adding the extra "fluff" to tell the machine what type of alias it is
 $TCPAlias = "DBMSSOCN," + $ServerName + "\" + $InstanceName
-#$NamedPipesAlias = "DBNMPNTW,\\" + $ServerName + "\pipe\" + "MSSQL$" + $InstanceName + "\sql\query"
+$NamedPipesAlias = "DBNMPNTW,\\" + $ServerName + "\pipe\" + "MSSQL$" + $InstanceName + "\sql\query"
  
 #Creating our TCP/IP Aliases
 #New-ItemProperty -Path $x86 -Name $AliasName -PropertyType String -Value $TCPAlias -Force
