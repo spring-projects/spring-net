f4bc6597771502afa07bfc6efb5524fcaa4f0ee0
Marko Lahma <marko.lahma@gmail.com>
2018-10-14 10:04:18 +0300

    add basic benchmark project

---
+++ test/Spring/Spring.Benchmark/ContainerBenchmark.cs
@@ -0,0 +1,73 @@
+#region License
+
+// /*
+//  * Copyright 2018 the original author or authors.
+//  *
+//  * Licensed under the Apache License, Version 2.0 (the "License");
+//  * you may not use this file except in compliance with the License.
+//  * You may obtain a copy of the License at
+//  *
+//  *      http://www.apache.org/licenses/LICENSE-2.0
+//  *
+//  * Unless required by applicable law or agreed to in writing, software
+//  * distributed under the License is distributed on an "AS IS" BASIS,
+//  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  * See the License for the specific language governing permissions and
+//  * limitations under the License.
+//  */
+
+#endregion
+
+using BenchmarkDotNet.Attributes;
+using Spring.Benchmark.Classes;
+using Spring.Context;
+using Spring.Context.Support;
+
+namespace Spring.Benchmark
+{
+    [MediumRunJob]
+    [MemoryDiagnoser]
+    public class ContainerBenchmark
+    {
+        private IApplicationContext container;
+
+        [Params(5_000)]
+        public int Iterations { get; set; }
+
+        [GlobalSetup]
+        public void GlobalSetup()
+        {
+            container = ContextRegistry.GetContext();
+        }
+
+        [Benchmark]
+        public bool ResolveTransient()
+        {
+            bool ok = true;
+            for (int i = 0; i < Iterations; i++)
+            {
+                var transient1 = (ITransient1) container.GetObject(typeof(ITransient1).FullName);
+                var transient2 = (ITransient2) container.GetObject(typeof(ITransient2).FullName);
+                var transient3 = (ITransient3) container.GetObject(typeof(ITransient3).FullName);
+                ok &= transient1 != null && transient2 != null && transient3 != null;
+            }
+
+            return ok;
+        }
+        
+        [Benchmark]
+        public bool ResolveSingleton()
+        {
+            bool ok = true;
+            for (int i = 0; i < Iterations; i++)
+            {
+                var transient1 = (ISingleton1) container.GetObject(typeof(ISingleton1).FullName);
+                var transient2 = (ISingleton2) container.GetObject(typeof(ISingleton2).FullName);
+                var transient3 = (ISingleton3) container.GetObject(typeof(ISingleton3).FullName);
+                ok &= transient1 != null && transient2 != null && transient3 != null;
+            }
+
+            return ok;
+        }
+    }
+}
---
+++ test/Spring/Spring.Benchmark/Program.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace Spring.Benchmark
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            BenchmarkSwitcher.FromAssembly(typeof(HybridSetBenchmark).Assembly).Run(args);
+        }
+    }
+}
