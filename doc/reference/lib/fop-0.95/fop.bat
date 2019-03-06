33da6f8ff363ba58b18dfae54a16613c828c74c8
Spring Operator <spring-operator@users.noreply.github.com>
2019-03-05 23:55:06 -0600

    URL Cleanup
    This commit updates URLs to prefer the https protocol. Redirects are not followed to avoid accidentally expanding intentionally shortened URLs (i.e. if using a URL shortener).
    
    # Fixed URLs
    
    ## Fixed Success
    These URLs were fixed successfully.
    
    * http://www.apache.org/licenses/LICENSE-2.0 migrated to:
      https://www.apache.org/licenses/LICENSE-2.0 ([https](https://www.apache.org/licenses/LICENSE-2.0) result 200).
    * http://www.appveyor.com/docs/services-databases migrated to:
      https://www.appveyor.com/docs/services-databases ([https](https://www.appveyor.com/docs/services-databases) result 301).

--- doc/reference/lib/fop-0.95/fop.bat
+++ doc/reference/lib/fop-0.95/fop.bat
@@ -6,7 +6,7 @@ REM  The ASF licenses this file to You under the Apache License, Version 2.0
 REM  (the "License"); you may not use this file except in compliance with
 REM  the License.  You may obtain a copy of the License at
 REM
-REM      http://www.apache.org/licenses/LICENSE-2.0
+REM      https://www.apache.org/licenses/LICENSE-2.0
 REM
 REM  Unless required by applicable law or agreed to in writing, software
 REM  distributed under the License is distributed on an "AS IS" BASIS,
