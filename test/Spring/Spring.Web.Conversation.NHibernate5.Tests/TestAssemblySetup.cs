aa69071e41c9a1781f1ddd4f2b2beb0bdf66da2d
Marko Lahma <marko.lahma@gmail.com>
2021-04-25 20:48:41 +0300

    work around SQLite interop assembly loading and web request port problem

---
+++ test/Spring/Spring.Web.Conversation.NHibernate5.Tests/TestAssemblySetup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+
+namespace Spring
+{
+    [SetUpFixture]
+    public class TestAssemblySetup
+    {
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            // work around getting interop assembly to correct directory
+            var requiredFile = Path.Combine(Environment.CurrentDirectory, "SQLite.Interop.dll");
+            var fileToUse = Path.Combine(Environment.CurrentDirectory, "x86", "SQLite.Interop.dll");
+            if (!File.Exists(requiredFile) && File.Exists(fileToUse))
+            {
+                File.Copy(fileToUse, requiredFile);
+            }
+        }
+    }
+}
